3a25b7877cd5273f25aa8d99e7a674cf31256efb
Shorai no bijon <67965601+LU97h@users.noreply.github.com>
2022-04-17 10:57:55 +0200

    Update area_quadrato.py

--- area_quadrato.py
+++ area_quadrato.py
@@ -1,3 +1,3 @@
-lato = int(input("Inserisci il valore del lato: "))
-area = lato*lato
-print(area)
+lato = int(input("Inserisci il valore del lato: ")) #ho inserito una variabile chiamata "lato" dove al suo interno viene mostrato un input chiedendo di inserire il valore del lato e con la funzione "int" obblighi l'utente a inserire un numero intero
+area = lato*lato #ho scritto un'altra variabile chiamata "area" dove all'interno ho scritto la sua funzione per calcolare l'area del quadrato utilizzando la variabile "lato"
+print(area) #infine stampiamo la variabile "area" dove mostrerà il risultato
